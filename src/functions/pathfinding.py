f8b8773a372a38c2f5ef02173ac0a5cd7ce117a2
mhkr1898 <mike.kristensen@live.dk>
2023-04-30 16:11:57 +0000

    initproject

--- src/functions/pathfinding.py
+++ src/functions/pathfinding.py
@@ -1,9 +1,8 @@
 import os
-def cur_dir():
-    current_script_dir = os.path.dirname(os.path.abspath(__file__))
-    return current_script_dir
-def cur_script():
-    current_script_dir = os.path.dirname(os.path.abspath(__file__))
-    filename = os.path.basename(__file__)
-    loc = current_script_dir + "/" + filename
+def cur_dir() -> str:
+    return os.getcwd() + '/'
+def cur_script() -> str:
+    current_script_dir: str = os.path.dirname(os.path.abspath(__file__))
+    filename: str = os.path.basename(__file__)
+    loc: str = current_script_dir + "/" + filename
     return loc
